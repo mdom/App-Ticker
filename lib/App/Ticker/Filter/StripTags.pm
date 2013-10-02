df6ec13b581e4c30d40f8592b5e687e5874e2e1b
Mario Domgoergen <mdom@taz.de>
2013-10-02 13:34:52 +0200

    eventify lib/App/Ticker/Filter/StripTags.pm

--- lib/App/Ticker/Filter/StripTags.pm
+++ lib/App/Ticker/Filter/StripTags.pm
@@ -6,23 +6,24 @@ with 'App::Ticker::Role::DomainRule';
 use Mojo::DOM;
 
 sub process_item {
-    my ($self,$item) = @_;
-    my $rule  = $self->get_rule($item);
-    return if ! $rule;
+    my ( $self, $item, $cb ) = @_;
+    if ( my $rule = $self->get_rule($item) ) {
 
-    my $dom = Mojo::DOM->new($item->body);
+        my $dom = Mojo::DOM->new( $item->body );
 
-    if ( exists $rule->{strip}  ) {
-        for my $strip ( @{ $rule->{strip} } ) {
-            $dom->find($strip)->pluck('remove');
+        if ( exists $rule->{strip} ) {
+            for my $strip ( @{ $rule->{strip} } ) {
+                $dom->find($strip)->pluck('remove');
+            }
         }
-    }
-    if ( exists $rule->{replace} ) {
-        for my $replace ( @{ $rule->{replace} } ) {
-            $dom->find( $replace->[0] )->pluck( 'replace', $replace->[1] );
+        if ( exists $rule->{replace} ) {
+            for my $replace ( @{ $rule->{replace} } ) {
+                $dom->find( $replace->[0] )->pluck( 'replace', $replace->[1] );
+            }
         }
+        $item->body( $dom->to_xml );
     }
-    $item->body( $dom->to_xml );
+    $cb->($item);
     return;
 }
 
