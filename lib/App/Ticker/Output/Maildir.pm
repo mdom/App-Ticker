406fe0061a7a4cfd5a57036db3c7d474d42bd501
Mario Domgoergen <mdom@taz.de>
2013-10-04 17:11:32 +0200

    new output plugin App::Ticker::Output::Maildir added

---
+++ lib/App/Ticker/Output/Maildir.pm
@@ -0,0 +1,25 @@
+package App::Ticker::Output::Maildir;
+use Moo;
+extends 'App::Ticker::Plugin';
+with 'App::Ticker::Role::FormatMail';
+
+use Mojo::ByteStream 'b';
+use Try::Tiny;
+use Path::Tiny;
+use Maildir::Lite;
+
+has maildir => ( is => 'rw', required => 1 );
+
+sub process_item {
+	my ($self,$item,$cb) = @_;
+	my $msg = $self->format_mail($item);
+	my $mail = $msg->as_string;
+	my $mdir = Maildir::Lite->new( dir=> $self->maildir );
+	if ( $mdir->creat_message($mail) ) {
+		warn "create message in maildir failed for " . $item->title . "\n";
+	}
+	$cb->($item);
+	return;
+}
+
+1;
